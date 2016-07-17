609cbaff14ff8ce80e56bcb2f38665f81de57f63
Hans Miikael Uuspõld <hans.miikael@gmail.com>
2016-07-17 13:03:08 +0300

    Added scene navigation.

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+	//Just using it to load a new level.
+	public void LoadLevel(string name) {
+		Debug.Log("Level load requested for: " + name);
+		Application.LoadLevel(name);
+	}
+
+	public void QuitRequest() {
+		Application.Quit();
+	}
+}
