010f293e6af51089243b904d4a34dbef56fec5c7
Hans Miikael Uuspõld <hans.miikael@gmail.com>
2016-07-17 14:01:05 +0300

    Finished Number Wizard with level navigation, random variable and changed text.

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -4,7 +4,6 @@ using System.Collections;
 public class LevelManager : MonoBehaviour {
 	//Just using it to load a new level.
 	public void LoadLevel(string name) {
-		Debug.Log("Level load requested for: " + name);
 		Application.LoadLevel(name);
 	}
 
---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class NumberWizard : MonoBehaviour {
+
+	private int max;
+	private int min;
+	private int guess;
+
+	public int maxGuessesAllowed = 5;
+	public Text guessText;
+
+	void Start () {
+		StartGame();
+    }
+
+	void StartGame() {
+		max = 1000;
+		min = 1;
+		guess = Random.Range(min, max+1);
+	}
+
+	public void GuessHigher() {
+		min = guess;
+		NextGuess();
+	}
+
+	public void GuessLower() {
+		max = guess;
+		NextGuess();
+	}
+
+	void NextGuess() {
+		guess = Random.Range(min, max+1);
+		guessText.text = guess.ToString();
+		maxGuessesAllowed--;
+
+		if (maxGuessesAllowed <= 0) {
+			Application.LoadLevel("Win");
+		}
+	}
+}
